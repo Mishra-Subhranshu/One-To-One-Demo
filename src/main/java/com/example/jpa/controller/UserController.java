935080134f1e7e9174af8dfedc13a257a68d1895
mishra-subhranshu <mishrasubhranshu2@gmail.com>
2021-09-10 18:34:04 +0530

    committed

--- src/main/java/com/example/jpa/controller/UserController.java
+++ src/main/java/com/example/jpa/controller/UserController.java
@@ -1,12 +1,12 @@
 package com.example.jpa.controller;
 
 import com.example.jpa.model.User;
+import com.example.jpa.model.UserProfile;
+import com.example.jpa.repository.UserProfileRepository;
+import com.example.jpa.repository.UserRepository;
 import com.example.jpa.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,6 +15,12 @@ public class UserController {
 
     @Autowired
     UserService userService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    UserProfileRepository userProfileRepository;
 
     @GetMapping("/get")
     public List<User> getAllUsers() {
@@ -25,5 +31,35 @@ public class UserController {
     public User postAllUsers(@RequestBody User user) {
         System.out.println("controller");
         return userService.postAllUsers(user);
+    }
+
+    @PutMapping("/user/{id}")
+    public User updateEmployee(@RequestBody User user,@PathVariable long id) {
+        return userRepository.findById(id)
+                .map(user1 -> {
+                    user1.setFirstName(user.getFirstName());
+                    user1.setLastName(user.getLastName());
+                    user1.setEmail(user.getEmail());
+                    return userRepository.save(user1);
+                })
+                .orElseGet(() -> {
+                    user.setId(id);
+                    return userRepository.save(user);
+                });
+    }
+
+    @PutMapping("/userProfile/{id}")
+    public UserProfile updateEmployee(@RequestBody UserProfile userProfile,@PathVariable long id) {
+        return userProfileRepository.findById(id)
+                .map(user1 -> {
+                    user1.setAddress(userProfile.getAddress());
+                    user1.setDateOfBirth(userProfile.getDateOfBirth());
+                    user1.setPhoneNumber(userProfile.getPhoneNumber()); ;
+                    return userProfileRepository.save(user1);
+                })
+                .orElseGet(() -> {
+                    userProfile.setId(id);
+                    return userProfileRepository.save(userProfile);
+                });
     }
 }
